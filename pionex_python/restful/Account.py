81bf27354c4974f11a766af7109b7a2e2d108d67
Alejandro Rodríguez Moreno <mrsh4d0w@proton.me>
2025-03-25 16:27:37 +0100

    Add files via upload

--- pionex_python/restful/Account.py
+++ pionex_python/restful/Account.py
@@ -4,5 +4,13 @@ class Account(RestClient):
     def __init__(self, key, secret):
         super().__init__(key, secret)
 
-    def get_balance(self):
-        return self._send_request('GET', '/api/v1/account/balances')
+    def get_balance(self, coin=None):
+        if coin is None:
+            return self._send_request('GET', '/api/v1/account/balances')
+        else:
+            result = self._send_request('GET', '/api/v1/account/balances')
+            return next(
+                (balance['free'] for balance in result['data']['balances'] if balance['coin'] == coin), 
+                0
+            )
+        
